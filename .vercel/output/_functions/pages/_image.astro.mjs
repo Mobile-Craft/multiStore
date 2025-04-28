ff7d455d9bb65c434d138eed53122e96dfcf2d88
Elder Moises Tavarez Gomez <emtavarez@banreservas.com>
2025-04-27 22:45:47 -0400

    creando el wrangler

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DiOjMbwy.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 
-import vercel from '@astrojs/vercel/serverless';
 
 import react from '@astrojs/react';
 
@@ -17,8 +16,7 @@ export default defineConfig({
   //   edgeMiddleware: true,
   // }),
   integrations: [tailwind(), react()],
-  adapter: cloudflare({
-    imageService: 'cloudflare'
-  }),
+  adapter: cloudflare(),
   output: 'server',
+
 });
