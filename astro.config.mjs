0f3d9fb76aeaa15295b8e4fce47cef14e08f4f52
Elder Moises Tavarez Gomez <emtavarez@banreservas.com>
2025-04-28 22:38:20 -0400

    configuracion para despliegue ssr lista

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,15 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
 
   integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
